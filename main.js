fefcbbb400ca65ad850a3f8d8da06240aa1b7341
Ninjabear <83464653+igrady2@users.noreply.github.com>
2021-11-03 10:22:12 -0400

    add typeof(keyName) to console

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const inputField = document.getElementById('input');
 //input listeners
 document.addEventListener('keypress', (event) => {
     const keyName = event.key;
-    console.log(keyName)
+    console.log(`${keyName} TypeOf: ${typeof(keyName)}`)
 });
