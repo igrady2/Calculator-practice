cc94a888cccdfb28b33af975d7b308f53a496cce
Ninjabear <83464653+igrady2@users.noreply.github.com>
2021-11-03 11:00:57 -0400

    add function buttonListeners and call it

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const inputField = document.getElementById('input');
-const acceptedKeys = ['1','2','3','4','5','6','7','8','9','0','x','X','*','(',')','-','+','=','/','Enter'];
+const acceptedKeysExact = ['1','2','3','4','5','6','7','8','9','0','*','(',')','-','+','/','Enter','C'];
+const acceptedKeysAproximate = ['x','X','=','c'];
+console.log(inputField)
 
 //input listeners
 document.addEventListener('keypress', (event) => {
     const keyName = event.key;
-    console.log(`${keyName} TypeOf: ${typeof(keyName)}`)
-    if (acceptedKeys.includes(keyName)) {
+    if (acceptedKeysExact.includes(keyName) || acceptedKeysAproximate.includes(keyName)) {
         console.log(keyName);
     } else {
         console.log('Not valid key');
     }
 });
+
+function buttonListeners(array) {
+    const assign = (input) => {
+        let button = document.getElementById(`${input}`);
+        button.addEventListener('click', (event) => {
+            inputField.value.concat(`${input}`);
+        })
+    }
+    array.forEach(element => assign(element));
+}
+
+buttonListeners(acceptedKeysExact);
