0f12ff5cc335734c1b2d87642de88dc486593dfa
Ninjabear <83464653+igrady2@users.noreply.github.com>
2021-11-03 12:45:54 -0400

    change id 'back' to 'Backspace' add new button objects to button array and assign them listeners

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 const inputField = document.getElementById('input');
-const acceptedKeysExact = ['1','2','3','4','5','6','7','8','9','0','*','(',')','-','+','/','.'];
-const acceptedKeysAproximate = ['x','X','=','c','Enter','C'];
+const acceptedKeysExact = ['1','2','3','4','5','6','7','8','9','0','*','(',')','-','+','/','.','Enter','C','Backspace'];
+const acceptedKeysAlternate = ['x','X','=','c'];
 let buttonArray = [];
 
 
-//assigns button variable
+//assigns button variables to buttonArray
 function assignButtonArray(lOL) {
     buttonArray.push(document.getElementById(lOL));
 }
 //assigns button variable listeners
 function assignButtonListeners() {
-    for (let i = 0; i < acceptedKeysExact.length; i++) {
+    for (let i = 0; i < acceptedKeysExact.length-3; i++) {
         buttonArray[i].addEventListener('click', (event) => {
             input.innerHTML += acceptedKeysExact[i]; 
-        })
+        });
     }
+    buttonArray[acceptedKeysExact.indexOf('Enter')].addEventListener('click', (event) => {
+        console.log('input function to compute answer here')
+    });
+    buttonArray[acceptedKeysExact.indexOf('C')].addEventListener('click', (event) => {
+        input.innerHTML = '';
+    });
+    buttonArray[acceptedKeysExact.indexOf('Backspace')].addEventListener('click', (event) => {
+        input.innerHTML =input.innerHTML.slice(0,-1)
+    });
 }
+
 //calls above functions
 acceptedKeysExact.forEach(element => assignButtonArray(element));
 assignButtonListeners();
@@ -23,12 +33,15 @@ assignButtonListeners();
 //input listeners
 document.addEventListener('keypress', (event) => {
     const keyName = event.key;
-    if (acceptedKeysExact.includes(keyName) || acceptedKeysAproximate.includes(keyName)) {
-        //console.log(keyName);
+    if (acceptedKeysExact.includes(keyName) || acceptedKeysAlternate.includes(keyName)) {
+        
         if (acceptedKeysExact.includes(keyName)) {
             buttonArray[acceptedKeysExact.indexOf(keyName)].click();
+        
         } else {
+        
             switch(keyName) {
+        
                 case 'x':
                 case 'X':
                     buttonArray[acceptedKeysExact.indexOf('*')].click();
@@ -36,7 +49,6 @@ document.addEventListener('keypress', (event) => {
                 default:
                     console.log('Key not set yet')
             }
-
         }
     } else {
         console.log('Not valid key');
