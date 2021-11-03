416ae5d7fcf8595a574aea9b05ff0d15ae4724c7
Ninjabear <83464653+igrady2@users.noreply.github.com>
2021-11-03 10:32:54 -0400

    check keypress against acceptedKeys

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const inputField = document.getElementById('input');
+const acceptedKeys = ['1','2','3','4','5','6','7','8','9','0','x','X','*','(',')','-','+','=','/','Enter'];
 
 //input listeners
 document.addEventListener('keypress', (event) => {
     const keyName = event.key;
     console.log(`${keyName} TypeOf: ${typeof(keyName)}`)
+    if (acceptedKeys.includes(keyName)) {
+        console.log(keyName);
+    } else {
+        console.log('Not valid key');
+    }
 });
