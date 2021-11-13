dff3978c60e0502463167e3ce9e6f11dcd2bdf61
Ninjabear <83464653+igrady2@users.noreply.github.com>
2021-11-13 17:18:51 -0400

    const => let for strLength in addImpliedX

--- modules/parser.js
+++ modules/parser.js
@@ -64,7 +64,7 @@ const operatorMain = (num1,operatorStr,num2) => {
 
 const addImpliedX = (arr) => {
     let workingArr = arr;
-    const strLength = workingArr.length;
+    let strLength = workingArr.length;
     console.log(`before addImpliedX ${workingArr}`)
         
     for (let i = 1; i < strLength; i++) {
